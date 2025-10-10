982280a084e49c2afb64d9a8da90019f807b0c7e
joaovkm06 <joaosports19@gmail.com>
2025-10-10 16:34:56 -0400

    kkkkkk

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
+// server.js
 import app from "./src/app.js";
+import connectDB from "./src/db.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const PORT = process.env.PORT || 4000;
 
-app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta http://localhost:${PORT}`);
-});
+const startServer = async () => {
+  try {
+    await connectDB(); // conecta no MongoDB antes de iniciar
+    app.listen(PORT, () => {
+      console.log(`Servidor rodando em http://localhost:${PORT}`);
+    });
+  } catch (err) {
+    console.error("Erro ao iniciar servidor:", err);
+  }
+};
+
+startServer();
