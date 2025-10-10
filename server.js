625f6b563acbe9a972642cff13aa56d7b63458e2
joaovkm06 <joaosports19@gmail.com>
2025-10-10 16:39:13 -0400

    kiuy

--- server.js
+++ server.js
@@ -1,21 +1,5 @@
-// server.js
-import app from "./src/app.js";
-import connectDB from "./src/db.js";
-import dotenv from "dotenv";
+// api/index.js (ou api/jogadores.js)
+import app from "../src/app.js";
+import serverless from "serverless-http";
 
-dotenv.config();
-
-const PORT = process.env.PORT || 4000;
-
-const startServer = async () => {
-  try {
-    await connectDB(); // conecta no MongoDB antes de iniciar
-    app.listen(PORT, () => {
-      console.log(`Servidor rodando em http://localhost:${PORT}`);
-    });
-  } catch (err) {
-    console.error("Erro ao iniciar servidor:", err);
-  }
-};
-
-startServer();
+export const handler = serverless(app);
