d8c4d5bc877005ce66bfd8e4285472a1c7b6eade
joaovkm06 <joaosports19@gmail.com>
2025-10-10 17:58:20 -0400

    erro no import

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 import serverless from "serverless-http";
 import express from "express";
-import scoreRoutes from "./src/routes/scoreRoutes.js";
+import scoreRoutes from "../src/routes/scoreRoutes.js";
 import swaggerUi from "swagger-ui-express";
 import YAML from "yamljs";
 import path from "path";
