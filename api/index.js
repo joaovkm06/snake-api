751d82393cfe856bde7d0551cc01c73e895b30c0
joaovkm06 <joaosports19@gmail.com>
2025-10-10 16:21:58 -0400

    tetando coisas

--- api/index.js
+++ api/index.js
@@ -1,16 +1,4 @@
-import serverless from "serverless-http";
-import app from "../src/app.js";
-import connectDB from "../src/db.js";
+import serverless from 'serverless-http';
+import app from '../Snake-API/src/app.js';
 
-// Conecta no MongoDB antes de responder
-const handler = async (req, res) => {
-  try {
-    await connectDB();
-    return app(req, res);
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ message: "Erro ao conectar no banco" });
-  }
-};
-
-export const defaultHandler = serverless(handler);
+export const handler = serverless(app);
