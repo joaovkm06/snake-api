92c9e48597e1eecd8a7195f238a2b989711b702e
joaovkm06 <joaosports19@gmail.com>
2025-10-10 15:58:49 -0400

    tive que mudar a concordancia do app e do dbconect

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
 import express from "express";
 import cors from "cors";
-import mongoose from "mongoose";
 import dotenv from "dotenv";
 import scoreRoutes from "./routes/scoreRoutes.js"; 
 import swaggerUi from "swagger-ui-express";
 import YAML from "yamljs";
 import path, { dirname, join } from "path";
 import { fileURLToPath } from "url";
+import connectDB from "./db.js"; // importa o db com cache
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -19,9 +19,16 @@ const app = express();
 app.use(cors());
 app.use(express.json()); 
 
-mongoose.connect(process.env.MONGODB_URI)
-.then(() => console.log("MongoDB conectado"))
-.catch(err => console.error("Erro ao conectar no MongoDB:", err));
+
+app.use(async (req, res, next) => {
+  try {
+    await connectDB();
+    next();
+  } catch (err) {
+    console.error("Erro na conexão com o MongoDB:", err);
+    res.status(500).json({ error: "Erro ao conectar ao banco" });
+  }
+});
 
 app.use("/", scoreRoutes);
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
--- src/config/DbConnect.js
+++ src/config/DbConnect.js
@@ -1,16 +1,23 @@
 import mongoose from "mongoose";
 
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
 const connectDB = async () => {
-	try {
-		await mongoose.connect(process.env.MONGO_URI, {
-			useNewUrlParser: true,
-			useUnifiedTopology: true,
-		});
-		console.log("MongoDB conectado com sucesso!");
-	} catch (error) {
-		console.error("Erro ao conectar ao MongoDB:", error);
-		process.exit(1);
-	}
+  if (cached.conn) return cached.conn;
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }).then((mongoose) => mongoose);
+  }
+
+  cached.conn = await cached.promise;
+  return cached.conn;
 };
 
 export default connectDB;
